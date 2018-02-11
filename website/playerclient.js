b7625686f53f0fc45a4588071fc2cd785d356b04
sjfricke <sjfricke@wisc.edu>
2018-02-11 06:39:21 -0600

    init new web

---
+++ website/playerclient.js
@@ -0,0 +1,107 @@
+"use strict";
+
+$(document).ready(function() {
+    // wait for DOM to load
+    setWebSocket();
+    setup();
+});
+
+// Either in Selection state or play state
+// selection state == 0 (default)
+// play state == 1
+var g_state = 0;
+
+var SongList = [
+  "Song 1",
+  "Song 2",
+  "Song 3",
+  "Song 4",
+  "Song 5",
+  "Song 6",
+  "Song 7",
+  "Song 8"
+  ]
+
+var clientID = 0;
+var currentSong = 0;
+
+var piemenu;
+
+function setWebSocket() {
+  // Attempts to just reload webpage if it was not able to get websocket
+  // Will cause loop if not connect, but app is useless anyways without WS
+  try {
+   webSocket = new WebSocket('ws://' + location.host);
+   webSocket.onmessage = wsOnMessage;
+  } catch (e) {
+   location.reload();
+  }
+}
+
+function nextsong(){
+
+  currentSong++;
+
+  if (currentSong >= SongList.length) {
+    currentSong = 0;
+  }
+
+  piemenu.navigateWheel(currentSong);
+  progress(currentSong*10);
+  //skiptosong(currentSong);
+
+  // broadcast(1, currentSong);
+}
+
+function prevsong(){
+
+  currentSong--;
+  if (currentSong < 0) {
+    currentSong = SongList.length - 1;
+  }
+
+  console.log(currentSong);
+  //skiptosong(currentSong);
+
+  piemenu.navigateWheel(currentSong);
+  progress(currentSong*10);
+
+  // broadcast(1, currentSong);
+}
+
+function progress(timeleft) {
+  var Timer;
+  Timer = new radialTimer();
+  Timer.init("timer", timeleft);
+
+};
+
+function setup() {
+
+    piemenu = new wheelnav('piemenu');
+    piemenu.clockwise = false;
+    piemenu.sliceInitPathFunction = piemenu.slicePathFunction;
+    piemenu.initPercent = 0.1;
+    piemenu.wheelRadius = piemenu.wheelRadius * 0.83;
+    piemenu.navItemsContinuous = true;
+    // piemenu.sliceAngle = 22.5; for partial
+    piemenu.createWheel(SongList);
+    piemenu.setTooltips(['0','1','2','3','4','5','6','7']);
+    
+}
+
+function skiptosong(num){
+  currentSong=parseInt(num);
+  progress(currentSong*10);
+}
+
+function stupid(){
+}
+
+function addaudio(){
+
+}
+
+function stopallaudio(){
+
+}
---
+++ website/websocket.js
@@ -0,0 +1,90 @@
+// global WebSocket pointer
+var webSocket;
+
+// Used to package values to be sent down to C
+function broadcast(key, ...values) {
+    if (isNaN(key )) { return false; }
+    webSocket.send(key + ":" + values.join(":"));
+};
+
+// decides what do when message arrives
+function wsOnMessage(event) {
+console.log(event);
+  // Message looks like => { "type" : 1, "value" : 0 }
+  var message = JSON.parse(event.data);
+
+  if (g_state == 0) { // Selection
+    switch(parseInt(message.type)) {
+    case 0: // Red button
+      break;
+    case 1: // Yellow Button  
+      break;
+    case 2: // Green Button
+      break;
+    case 3: // Jog Wheel Button
+      broadcast(3, currentSong);
+      break;
+    case 4: // Jog Wheel LEFT
+      prevsong();
+      break;
+    case 5: // Jog Wheel RIGHT
+      nextsong();
+      break;
+    case 6: // Volume
+      break;
+    default:
+      warn("WebSocket", "No case for data: %0", message);
+    }
+  } else if (g_state == 1) { // Play
+    switch(parseInt(message.type)) {
+    case 0: // Red button
+      break;
+    case 1: // Yellow Button  
+      break;
+    case 2: // Green Button
+      break;
+    case 3: // Jog Wheel Button
+      break;
+    case 4: // Jog Wheel LEFT
+      break;
+    case 5: // Jog Wheel RIGHT
+      break;
+    case 6: // Volume
+      break;
+    default:
+      warn("WebSocket", "No case for data: %0", message);
+    }
+  }
+
+  
+}
+
+/////////////////////////////////////
+// for testing to callback echo ws //
+/////////////////////////////////////
+
+function testLeft() {    
+    webSocket.send('{"type":4,"value":0}');
+}
+function testRight() {    
+    webSocket.send('{"type":5,"value":0}');
+}
+
+
+
+      // case "play":
+      //   var btn=document.getElementById("play");
+      //   play.classList="";
+      //   play.classList="playBtn";
+      //   sendToServer("play "+currentSong);
+      //   break;
+        
+      // case "pause":
+      //   var btn=document.getElementById("play");
+      //   play.classList="";
+      //   play.classList="pauseBtn";
+      //   sendToServer("pause "+currentSong);
+      //   break;
+
+      // case "skip":
+      //   progress(10);
